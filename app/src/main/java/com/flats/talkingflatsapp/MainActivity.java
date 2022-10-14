8dc9e5571d7f23004c9dd08d99c9a27cf2c3e88c
SashaPotemkin <alex.potjomkin@gmail.com>
2022-10-14 19:40:33 +0300

    Карта с местоположением пользователя

--- app/src/main/java/com/flats/talkingflatsapp/MainActivity.java
+++ app/src/main/java/com/flats/talkingflatsapp/MainActivity.java
@@ -4,11 +4,60 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
+
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_welcome);
+
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                try {
+                    sleep(3000);
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+                finally {
+                    PermissionListener permissionlistener = new PermissionListener() {
+                        @Override
+                        public void onPermissionGranted() {
+                            Intent intent = new Intent(MainActivity.this, UserLocationActivity.class);
+                            startActivity(intent);
+                        }
+
+                        @Override
+                        public void onPermissionDenied(List<String> deniedPermissions) {
+                            Toast.makeText(MainActivity.this, "Доступ запрещен\n" + deniedPermissions.toString(), Toast.LENGTH_SHORT).show();
+                        }
+
+
+                    };
+
+                    TedPermission.with(MainActivity.this)
+                            .setPermissionListener(permissionlistener)
+                            .setDeniedMessage("Для использования приложения необходимо предоставить доступ к местоположению")
+                            .setPermissions( Manifest.permission.ACCESS_FINE_LOCATION)
+                            .check();
+                }
+            }
+        };
+        thread.start();
+
     }
 }
